9fe3e499c9c098c6193768cfe471fcb70588e1b8
Antoine Weber <aweber@numesia.fr>
2018-12-07 10:40:02 +0100

    initial release

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,15 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+function getQuickPickItem(uri: any): any{
+    return {
+        scheme: 'file://',
+        detail: uri,//getRelativeFileName(uri.fsPath),
+        label: uri.split('/').pop(),//getFileName(uri.path),
+        path:uri
+    };
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -14,11 +23,37 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.locateFile', () => {
         // The code you place here will be executed every time your command is executed
+        let editor = vscode.window.activeTextEditor;
+        //let doc = editor.document;
+        if(editor){
+            let text = editor.document.getText(editor.selection);
+             // Display a message box to the user
+            vscode.window.showInformationMessage('Recherche en cours pour: ' + text );
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+            const cp = require('child_process');
+            cp.exec('locate '+text, (err:any, stdout:any, stderr:any) => {
+
+                let files = stdout.split("\n");
+                files = files.filter((s:string) => s.length>0).map((f:string) => getQuickPickItem(f));
+
+                vscode.window.showQuickPick(files,{
+                    matchOnDetail:true
+                })
+                .then((file:any)=>{
+                    if(file&&file.path){
+                        vscode.workspace.openTextDocument(file.path).then(txtDocument=>vscode.window.showTextDocument(txtDocument));
+                    }
+                });
+            });
+        }
+        else{
+            vscode.window.showInformationMessage('Veuillez selectionner un texte à rechercher.');
+        }
+        
+        
+
     });
 
     context.subscriptions.push(disposable);
